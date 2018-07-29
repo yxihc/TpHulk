6bdf69d40405ea096f25561d4e7177186574f4ac
shenkun <825210484@qq.com>
2018-07-29 16:57:08 +0800

    去除多余文件,把crash单拎出来

---
+++ sample/src/main/java/com/taopao/baseapp/base/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.taopao.baseapp.base;
+
+import android.databinding.ViewDataBinding;
+
+import com.taopao.mvvmbase.base.BaseMVVMActivity;
+import com.taopao.mvvmbase.base.BaseMVVMViewModel;
+
+/**
+ * @Author：淘跑
+ * @Date: 2018/7/29 16:09
+ * @Use：
+ */
+public abstract class BaseActivity<V extends ViewDataBinding, VM extends BaseMVVMViewModel> extends BaseMVVMActivity {
+}
